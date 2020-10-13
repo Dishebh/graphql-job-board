012614a3a84f20bf3290588e77bafc4a234ec43a
Dishebh Bhayana <dishebh27@gmail.com>
2020-10-13 12:22:51 +0530

    required user to be authenticated for createJob

--- server/server.js
+++ server/server.js
@@ -25,7 +25,9 @@ const typeDefs = gql(fs.readFileSync('./schema.graphql', { encoding: 'utf8' }));
 // Provide resolver functions for your schema fields
 const resolvers = require('./resolvers')
 
-const apolloServer = new ApolloServer({ typeDefs, resolvers })
+const context = ({ req }) => ({ user: req.user })
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers, context })
 
 apolloServer.applyMiddleware({app, path: '/graphql'});
 
